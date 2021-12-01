ec09dd18655162ee4d00fef5f83925a4b7bd52da
victoria-parker <victoriaparkerb@gmail.com>
2021-12-01 16:49:48 +0000

    some style added

---
+++ dashboard.php
@@ -0,0 +1,17 @@
+<?php
+    include 'includes/header.php';
+?>
+    <main class="container my-3">
+        <h1>Periodic Table Elements</h1>
+
+        <div class="border rounded-1 element-container">
+            <p class="text-end">1</p>
+            <p class="text-center fw-bold">H</p>
+            <p>Hydrogen</p>
+            <p>1.008</p>
+        </div>
+    </main>
+
+<?php
+    include 'includes/footer.html';
+?>
