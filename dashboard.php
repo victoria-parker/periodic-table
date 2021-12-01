e90ce9a8ea59b5c29c500fc179a1a63f2093552e
victoria-parker <victoriaparkerb@gmail.com>
2021-12-01 17:38:02 +0000

    new Element called in the main html and elements displayed in the html

--- dashboard.php
+++ dashboard.php
@@ -1,16 +1,23 @@
 <?php
     require 'config/config.php';
+    $Element=new Element();
+    $elements=$Element->listElements();
     include 'includes/header.php';
 ?>
     <main class="container my-3">
         <h1>Periodic Table Elements</h1>
-
+        <?php
+            foreach ($elements as $element){
+        ?>
         <div class="border rounded-1 element-container">
-            <p class="text-end">1</p>
-            <p class="text-center fw-bold">H</p>
-            <p>Hydrogen</p>
-            <p>1.008</p>
+            <p class="text-end"><?=$element['atomicNumber']?></p>
+            <p class="text-center fw-bold"><?=$element['symbol']?></p>
+            <p><?=$element['name']?></p>
+            <p><?=$element['atomicMass']?></p>
         </div>
+        <?php
+            }
+        ?>
     </main>
 
 <?php
