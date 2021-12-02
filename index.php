2c693e51a6f42e6b43889210cec67e7290f86858
victoria-parker <victoriaparkerb@gmail.com>
2021-12-02 14:16:25 +0000

    colors added

--- index.php
+++ index.php
@@ -9,15 +9,13 @@
         <h1 class="text-center">Periodic Table Elements</h1>
 
         <div id="periodic-table">
-
             <?php
                 foreach ($elements as $element){
             ?>
             <div class="border rounded-1 element-container <?=$element['groupBlock']?>" id="element<?=$element['atomicNumber']?>">
                 <p class="text-end"><?=$element['atomicNumber']?></p>
-                <p class="text-center fw-bold"><?=$element['symbol']?></p>
-                <p><?=$element['name']?></p>
-                <p><?=round($element['atomicMass'],2)?></p>
+                <p class="text-center fw-bolder fs-4"><?=$element['symbol']?></p>
+                <p class="text-center"><?=$element['name']?></p>
             </div>
             <?php
                 }
